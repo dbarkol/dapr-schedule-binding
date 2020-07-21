949c471b27985b9bf49aa3baf151efefe89784d2
dbarkol <dbarkol@hotmail.com>
2020-07-21 00:00:44 -0700

    parsing

--- go/app.go
+++ go/app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -12,11 +14,39 @@ func init() {
 	port = "8080"
 }
 
+type Joke struct {
+	Id    string `json:"id"`
+	Url   string `json:"url"`
+	Value string `json:"value"`
+}
+
 func main() {
 
-	// Return a collection of topic subscriptions
 	http.HandleFunc("/schedule-trigger", func(w http.ResponseWriter, r *http.Request) {
+
 		log.Println("trigger invoked")
+
+		var j Joke
+
+		response, err := http.Get("https://api.chucknorris.io/jokes/random?category=sport")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer response.Body.Close()
+
+		body, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(body, &j)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(j.Value)
 	})
 
 	fmt.Println("starting HTTP server....")
